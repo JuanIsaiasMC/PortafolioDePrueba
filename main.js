93365c2f5318f9e86026ec9e0d3bd0d1b20524e4
junimc <isaias.meza.conde@gmail.com>
2022-06-05 13:01:28 -0500

    deseablescroll

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const desableScroll = document.querySelector('body')
 const navToogle = document.querySelector('.nav__toogle')
 const navMenu = document.querySelector('.nav__menu')
 const hideLinks = document.querySelectorAll('.nav__menu a[href^="#"]')
@@ -5,18 +6,21 @@ const closeMenu = document.querySelector('.ul__button')
 
 navToogle.addEventListener('click', () => {
     navMenu.classList.toggle('nav__menu-visible')
+    desableScroll.classList.toggle('scroll')
 })
 
 
 hideLinks.forEach(menuLink => {
     menuLink.addEventListener('click', function() {
         navMenu.classList.remove('nav__menu-visible')
+        desableScroll.classList.remove('scroll')
     })
 })
 
 
 closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('nav__menu-visible')
+    desableScroll.classList.remove('scroll')
 })
 
 
